f701200bd3dd45a917b9fe98d9e48b31e134e06b
sajan siwa <siwasajan4@gmail.com>
2024-03-20 15:37:08 +0545

    modal pagge style

--- mangalmaya/src/App.jsx
+++ mangalmaya/src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Modal from './assets/modal/modal'
+import Modal from './modal/modal'
 
 function App() {
-  const [count, setCount] = useState(0)
-  
+
   return (
     <>
       <Modal/>
