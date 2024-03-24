ab93912d1ab84daf153037ce70c29281637aa1e0
sus53 <manandharsrijan53@gmail.com>
2024-03-24 08:30:34 +0545

    Added Card-Grant-Bankless

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
+<<<<<<< HEAD
 import Accordion from "./page/accordion/Accordion"
 import Typography from "./page/typography/Typography"
 import './app.scss'
 import Card from "./page/card/Card"
+=======
+import './App.css'
+
+>>>>>>> parent of 8819e92 (completed Accordion)
 function App() {
+  
 
   return (
+<<<<<<< HEAD
     <div className="app flex flex-col">
       <Card />
       <Accordion />
       <Typography />
     </div>
+=======
+    <>
+     
+    </>
+>>>>>>> parent of 8819e92 (completed Accordion)
   )
 }
 
