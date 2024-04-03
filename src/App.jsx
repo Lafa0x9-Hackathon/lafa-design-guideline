cb656f485ead2378c25208d06b8e6df0d7a9a67b
sudeepmahato16 <sudeepmahato16@gmail.com>
2024-04-03 14:12:02 +0545

    add tooltip

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import Progress from "./components/Progress";
+import Tooltip from "./components/Tooltip";
 
 const App = () => {
   return (
     <main className="flex items-center justify-center h-screen">
-      <Progress value={40}/>
+      <Tooltip content="This is tooltip">
+        <button type="button" className="border py-[10px] px-3 text-[12px] font-Libre-Franklin rounded-[4px] bg-gray-50 text-gray-600">
+          Hover me
+        </button>
+      </Tooltip>
     </main>
   );
 };
---
+++ src/components/Tooltip/index.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+const fadeIn = {
+  hidden: {
+    opacity: 0,
+    transition: {
+      duration: 0.2,
+      ease: "easeOut",
+    },
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 0.25,
+      ease: "easeOut",
+    },
+  },
+};
+
+const Tooltip = ({ children, content }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div className="relative">
+      <div
+        onMouseOver={() => setIsVisible(true)}
+        onMouseLeave={() => setIsVisible(false)}
+      >
+        {children}
+      </div>
+
+      <AnimatePresence>
+        {isVisible ? (
+          <motion.div
+            variants={fadeIn}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className={
+              "absolute top-[130%] left-1/2 -translate-x-1/2 rounded-[4px] bg-[#0E0333]"
+            }
+          >
+            <div className="absolute top-[0.5px] left-1/2 -translate-x-1/2 -translate-y-full triangle" />
+            <p className="text-[12px] font-Libre-Franklin leading-[16px] text-[#fff] px-3 py-[10px] whitespace-nowrap">
+              {content}
+            </p>
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default Tooltip;
