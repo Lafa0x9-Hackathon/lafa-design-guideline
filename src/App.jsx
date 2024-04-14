b2e71a5092c669f34d62b0f059584101acc08a46
sajan siwa <siwasajan4@gmail.com>
2024-04-14 21:42:27 +0545

    mods

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import { Tabs, TabsContent, TabsTrigger, TabsList } from "./components/Tabs";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Tab from "./page/tabs/tabs";
 
-const App = () => {
+
+const router = createBrowserRouter([
+  {
+    path: "/tabs",
+    element: <Tab />,
+  },
+]);
+
+function App() {  
   return (
-    <main className="flex items-center justify-center h-screen">
-      <Tabs defaultValue="grants" className="w-[510px]">
-        <TabsList>
-          <TabsTrigger value="grants">Grants</TabsTrigger>
-          <TabsTrigger value="collections">Collections</TabsTrigger>
-          <TabsTrigger value="matchingPools">Matching Pools</TabsTrigger>
-        </TabsList>
-
-        <TabsContent value="grants">Grants</TabsContent>
-        <TabsContent value="collections">Collections</TabsContent>
-        <TabsContent value="matchingPools">Matching Pools</TabsContent>
-      </Tabs>
-    </main>
-  );
-};
+    <RouterProvider router={router}/>
+  )
+}
 
 export default App;
